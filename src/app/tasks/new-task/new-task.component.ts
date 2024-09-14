b6daa69bb54d18187f4b2ea2deea672039d3fc4b
https://gitlab.isoad.isogmbh.de/jln <jeanlambert.niyigaba@iso-gruppe.com>
2024-09-14 17:40:20 +0200

    dependency injection frist step

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -9,9 +10,18 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
+
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
+  // private tasksService: TasksService //this goes with the local instance
+
+  constructor(private tasksService: TasksService) {
+    // this.tasksService = new TasksService();// this has an issue of not having a local instance instead of general instance
+
+  }
 
   onAddTask(title: string, description: string) {
+    this.tasksService.addTask({title: title, description: description});
+    console.log('task', title)
     this.formEl()?.nativeElement.reset();
   }
 }
